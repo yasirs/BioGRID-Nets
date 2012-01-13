ba2e386b8da40f59653d3410608079f55c7d4047
Yasir Suhail <yasir.suhail@gmail.com>
2012-01-13 17:45:31 -0500

    get twice confirmed interactions

--- getNets.py
+++ getNets.py
@@ -1,22 +1,34 @@
-import os
+import os,urllib
+import networkx as nx
 
 BGRID_VERSION = '3.1.84'
 
 def download_zip(ver=BGRID_VERSION):
 	"""Download a .zip file from BioGRID
-	download_zip(ver='3.1.79')
 	"""
 	os.chdir('data')
 	if not os.path.exists(ver):
 		os.mkdir(ver)
 	os.chdir(ver)
-	urllib.urlretrieve('http://thebiogrid.org/downloads/archives/Release%20Archive/BIOGRID-3.1.79/BIOGRID-ORGANISM-%s.tab2.zip' %ver)
+	urllib.urlretrieve('http://thebiogrid.org/downloads/archives/Release%%20Archive/BIOGRID-%s/BIOGRID-ORGANISM-%s.tab2.zip' %(ver,ver),filename='BIOGRID-ORGANISM-%s.tab2.zip' %ver)
 	os.chdir('../../')
 
 def download_exists(ver):
 	return os.path.exists('data/'+ver)
 
-def getSpecies(species,int_type='physical',ver=BGRID_VERSION,id_type='entrez',as_Graph=False):
+def getMultiEdges(G,as_Graph=False,minConfirm=2):
+	if (type(G) != nx.MultiGraph):
+		raise TypeError("need a NetworkX MultiGraph")
+	GG = nx.Graph()
+	for e in G.edges_iter():
+		if (len(G.edge[e[0]][e[1]]) >= minConfirm):
+			GG.add_edge(e[0],e[1])
+	if as_Graph:
+		return GG
+	else:
+		return GG.edges()
+
+def getSpecies(species,int_type='physical',ver=BGRID_VERSION,id_type='entrez',as_Graph=False,as_MultiGraph=False):
 	if not download_exists(ver):
 		download_zip(ver)
 	os.chdir('data/'+ver+'/')
@@ -25,9 +37,9 @@ def getSpecies(species,int_type='physical',ver=BGRID_VERSION,id_type='entrez',as
 	if not os.path.exists('BIOGRID-ORGANISM-%s-%s.tab2.txt' %(species,ver)):
 		raise IOError("Species %s does not exist in BIOGRID-ORGANISM-%s.tab2.zip" %(species,ver))
 	os.chdir('../../')
-	return getFileNet('data/%s/BIOGRID-ORGANISM-%s-%s.tab2.txt' %(ver,species,ver),int_type=int_type,id_type=id_type,as_Graph=as_Graph)
+	return getFileNet('data/%s/BIOGRID-ORGANISM-%s-%s.tab2.txt' %(ver,species,ver),int_type=int_type,id_type=id_type,as_Graph=as_Graph,as_MultiGraph=as_MultiGraph)
 
-def getFileNet(fname,int_type='physical',id_type='entrez',as_Graph=False):
+def getFileNet(fname,int_type='physical',id_type='entrez',as_Graph=False,as_MultiGraph=False):
 	if int_type not in ('physical','genetic'):
 		raise ValueError("interaction types have to be physical or genetic")
 	if id_type not in ('entrez','official'):
@@ -40,7 +52,10 @@ def getFileNet(fname,int_type='physical',id_type='entrez',as_Graph=False):
 	idA = filter(lambda x: id_type in header[x].upper() and 'A' in header[x].upper(),range(len(header)))[0]
 	idB = filter(lambda x: id_type in header[x].upper() and 'B' in header[x].upper(),range(len(header)))[0]
 	type_index = header.index('Experimental System Type')
-	G = nx.Graph()
+	if as_MultiGraph:
+		G = nx.MultiGraph()
+	else:
+		G = nx.Graph()
 	for line in it:
 		words = line.rstrip().split('\t')
 		if words[type_index]==int_type:
@@ -51,9 +66,9 @@ def getFileNet(fname,int_type='physical',id_type='entrez',as_Graph=False):
 	else:
 		return G.edges()
 
-def getSpeciesPruned(species,int_type='physical',ver=BGRID_VERSION,id_type='entrez',as_Graph=False):
+def getSpeciesPruned(species,int_type='physical',ver=BGRID_VERSION,id_type='entrez',as_Graph=False,as_MultiGraph=False):
 	import networkx as nx
-	G = getSpecies(species,ver=ver,int_type=int_type,id_type=id_type,as_Graph=True)
+	G = getSpecies(species,ver=ver,int_type=int_type,id_type=id_type,as_Graph=True,as_MultiGraph=as_MultiGraph)
 	G = nx.connected_component_subgraphs(G)[0]
 	while True:
 		bad_nodes = map(lambda (k,v) :k, filter(lambda (k,v) :v<2,G.degree_iter()))
@@ -66,8 +81,11 @@ def getSpeciesPruned(species,int_type='physical',ver=BGRID_VERSION,id_type='entr
 	else:
 		return G.edges()
 
-	
-		
+
+def getSpeciesPrunedConfirmed(species,int_type='physical',ver=BGRID_VERSION,id_type='entrez',as_Graph=False,minConfirm=2):
+	G = getSpeciesPruned(species,int_type=int_type,ver=BGRID_VERSION,id_type='entrez',as_Graph=True,as_MultiGraph=True)
+	return getMultiEdges(G,as_Graph=as_Graph,minConfirm=minConfirm)
+
 
 	
 
