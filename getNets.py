fc8595b61fc4b2fdc5272ef52700cb48db65c0cb
Yasir Suhail <yasir.suhail@gmail.com>
2012-04-04 18:00:11 -0400

    updates

--- getNets.py
+++ getNets.py
@@ -1,7 +1,7 @@
 import os,urllib
 import networkx as nx
 
-BGRID_VERSION = '3.1.84'
+BGRID_VERSION = '3.1.87'
 
 def download_zip(ver=BGRID_VERSION, dataFolder='data'):
 	"""Download a .zip file from BioGRID
@@ -10,10 +10,13 @@ def download_zip(ver=BGRID_VERSION, dataFolder='data'):
 		os.mkdir(dataFolder)
 	if not os.path.exists(dataFolder + '/' + ver):
 		os.mkdir(dataFolder + '/' + ver)
-	urllib.urlretrieve('http://thebiogrid.org/downloads/archives/Release%%20Archive/BIOGRID-%s/BIOGRID-ORGANISM-%s.tab2.zip' %(ver,ver),filename='%s/%s/BIOGRID-ORGANISM-%s.tab2.zip' %(data,ver,ver))
+	urllib.urlretrieve('http://thebiogrid.org/downloads/archives/Release%%20Archive/BIOGRID-%s/BIOGRID-ORGANISM-%s.tab2.zip' %(ver,ver),filename='%s/%s/BIOGRID-ORGANISM-%s.tab2.zip' %(dataFolder,ver,ver))
 
 def download_exists(ver, dataFolder='data'):
-	return os.path.exists(dataFolder+'/'+ver)
+	if os.path.exists(dataFolder+'/'+ver):
+		return os.path.exists('%s/%s/BIOGRID-ORGANISM-%s.tab2.zip -d %s/%s' %(dataFolder,ver,ver,dataFolder,ver))
+	else:
+		return False
 
 def getMultiEdges(G,as_Graph=False,minConfirm=2):
 	if (type(G) != nx.MultiGraph):
@@ -30,10 +33,9 @@ def getMultiEdges(G,as_Graph=False,minConfirm=2):
 def getSpecies(species,int_type='physical',ver=BGRID_VERSION,id_type='entrez',as_Graph=False,as_MultiGraph=False,dataFolder='data'):
 	if not download_exists(ver,dataFolder=dataFolder):
 		download_zip(ver,dataFolder=dataFolder)
-	os.chdir('data/'+ver+'/')
 	if not os.path.exists('%s/%s/BIOGRID-ORGANISM-%s-%s.tab2.txt' %(dataFolder,ver,species,ver)):
-		os.system('unzip %s/%s/BIOGRID-ORGANISM-%s.tab2.zip -d %s/%s' %(dataFolder,ver,ver,dataFolder,ver)
-	if not os.path.exists('%s\%s\BIOGRID-ORGANISM-%s-%s.tab2.txt' %(dataFolder,ver,species,ver)):
+		os.system('unzip %s/%s/BIOGRID-ORGANISM-%s.tab2.zip -d %s/%s' %(dataFolder,ver,ver,dataFolder,ver))
+	if not os.path.exists('%s/%s/BIOGRID-ORGANISM-%s-%s.tab2.txt' %(dataFolder,ver,species,ver)):
 		raise IOError("Species %s does not exist in BIOGRID-ORGANISM-%s.tab2.zip" %(species,ver))
 	return getFileNet('%s/%s/BIOGRID-ORGANISM-%s-%s.tab2.txt' %(dataFolder,ver,species,ver),int_type=int_type,id_type=id_type,as_Graph=as_Graph,as_MultiGraph=as_MultiGraph)
 
@@ -82,11 +84,30 @@ def getSpeciesPruned(species,int_type='physical',ver=BGRID_VERSION,id_type='entr
 
 def getSpeciesPrunedConfirmed(species,int_type='physical',ver=BGRID_VERSION,id_type='entrez',as_Graph=False,minConfirm=2):
 	G = getSpeciesPruned(species,int_type=int_type,ver=BGRID_VERSION,id_type='entrez',as_Graph=True,as_MultiGraph=True)
-	return getMultiEdges(G,as_Graph=as_Graph,minConfirm=minConfirm)
-
+	CG = getMultiEdges(G,as_Graph=True,minConfirm=minConfirm)
+	G = nx.connected_component_subgraphs(CG)[0]
+	while True:
+		bad_nodes = map(lambda (k,v) :k, filter(lambda (k,v) :v<2,G.degree_iter()))
+		if len(bad_nodes):
+			G.remove_nodes_from(bad_nodes)
+		else:
+			break
+	if as_Graph:
+		return G
+	else:
+		return G.edges()
 
 	
-
+def writeToFile(edgeList,fname):
+	if os.path.exists(fname):
+		inp = raw_input('%s exists, overwrite? [y]/n? ' %fname)
+		if 'N' in inp.upper():
+			return
+	fo = open(fname,'w')
+	for e in edgeList:
+		fo.write('%s\t%s\n' %(e[0],e[1]))
+	fo.close()
+		
 
 
 
